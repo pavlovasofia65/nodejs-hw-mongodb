4c1704a4bfad41b790fdd1a6ddb7c2823af8abd0
pavlovasofia65 <pavlovasofia65@gmail.com>
2025-06-05 10:07:11 +0300

    status updates

--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@ export const setupServer = () =>{
     app.get('/contacts', async (req, res) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
             message: "Successfully found contacts!",
             data: contacts,
         });
@@ -38,6 +39,7 @@ export const setupServer = () =>{
         const contact = await getContactById(contactId);
         if(!contact){
             res.status(404).json({
+                status: 404,
                 message: 'Contact not found',
             });
         };
