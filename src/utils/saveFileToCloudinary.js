a99b5535406860bf55831e1c9f1116a595fd92ec
pavlovasofia65 <pavlovasofia65@gmail.com>
2025-06-19 20:26:15 +0300

    again imports

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -2,7 +2,7 @@ import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
 
 import { getEnvVar } from './getEnvVar.js';
-import { CLOUDINARY } from '../constants.js';
+import { CLOUDINARY } from '../constants/index.js';
 
 cloudinary.v2.config({
     secret: true,
